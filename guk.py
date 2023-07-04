d3b6e171ff381328796f57c250d7cd260707a05e
Gaduputi Udaykiran <gaduputiuday3104@gmail.com>
2023-07-04 14:52:33 +0530

    guk.py

---
+++ guk.py
@@ -0,0 +1,25 @@
+
+# def add(x,y):
+#     sum=a+b
+#     print(f"sum is:",sum)
+#     return sum
+
+# a=int(input("enter the 1st number:"))
+# b=int(input("enter the 2nd number:"))
+# add(a,b)
+
+def power(x,y):
+  
+    result=a**b
+    print(f"power of given number is:",result)
+
+    return result
+
+a=int(input("enter the number:"))
+b=int(input("enter the power:"))
+power(a,b)
+
+
+
+
+
